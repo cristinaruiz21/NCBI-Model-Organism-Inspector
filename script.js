8dadc78409081104fa0a4b875fe35d4f2a26a06a
Jack Murphy <signal.space@protonmail.com>
2020-10-06 14:32:02 -0500

    db queries written and commented

--- script.js
+++ script.js
@@ -1,53 +1,47 @@
-let orgQueryID = ""
-let chosenProtein = ""
-let geneToStructLinks = ""
+let orgQueryID = "";
+let chosenProtein = "";
+let geneToStructLinks = "";
 
 // Get the genes for organism to be studied
 // TODO interpolate user choice of model organism
 $.ajax({
-    url: `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=gene&term=Tobacco_mosaic_virus&retmax=1000&retmode=JSON`,
+  url: `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=gene&term=Ateline_alphaherpesvirus_1[organism]&retmax=1000&retmode=JSON`,
+  method: "GET",
+}).then(function (response) {
+  console.log(response);
+  //!! Save the list of UIDs
+  orgQueryID = response.esearchresult.idlist;
+
+  // Get the names of the genes
+
+  $.ajax({
+    url: `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&term=Tobacco_mosaic_virus[organism]&ID=${orgQueryID}&retmode=JSON`,
     method: "GET",
   }).then(function (response) {
     console.log(response);
-    console.log(response.esearchresult.idlist);
-    // Save the list of UIDs
-    orgQueryID = response.esearchresult.idlist;
+    //!! This is the location of the list protein names
+    for(let i = 0; i <orgQueryID.length; i++) {
+        console.log(`Logging a name -- ${response.result[orgQueryID[i]].name}`);
+    }
+    // TODO User choice ID to be set here
+    // TODO on click event to call never ajax after user choice
+    chosenProtein = orgQueryID;
+    console.log(`Chosen protein ID = ${chosenProtein}`);
 
-    // Get the names of the genes
+    // Link the gene DB to the structure DB
 
     $.ajax({
-        url: `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&term=Tobacco_mosaic_virus&ID=${orgQueryID}&retmode=JSON`,
-        method: "GET",
-      }).then(function (response) {
-        console.log(response);
-        // TODO User choice ID to be set here
-        chosenProtein = orgQueryID[8];
-        console.log(`Chosen protein ID = ${chosenProtein}`);
+      url: `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=gene&db=structure&id=${chosenProtein}&retmode=JSON`,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      // !! This is a list of of links from gene to struct UIDs
+      geneToStructLinks = response.linksets[0].linksetdbs[0].links;
+      console.log(`Here are the links from structure db ${geneToStructLinks}`);
 
-        // Link the gene DB to the structure DB
-        
-        $.ajax({
-            url: `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=gene&db=structure&id=${chosenProtein}&retmode=JSON`,
-            method: "GET",
-          }).then(function (response) {
-            console.log(response);
-            geneToStructLinks = response.linksets[0].linksetdbs[0].links;
-            console.log(`Here are the links from structure db ${geneToStructLinks}`)
-            // Get structure for gene product
-            // TODO use structure UIDs to generate a iCn3D query
-            $.ajax({
-                url: `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=structure&id=${geneToStructLinks}&retmode=JSON`,
-                method: "GET",
-              }).then(function (response) {
-                console.log(response);
-                chosenProtein = response;
-                
-              });
-          });
-      });
+      // Get structure for gene product
+      // TODO use structure UIDs to generate a iCn3D query
+      // TODO Render iCn3D window
+    });
   });
-  
-  
-  
-  
- 
+});
