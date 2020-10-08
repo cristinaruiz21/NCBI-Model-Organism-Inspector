07b290efafde07626d0332028874e81c86f46578
Jack Murphy <signal.space@protonmail.com>
2020-10-08 10:34:17 -0500

    change query sequence

--- assets/script.js
+++ assets/script.js
@@ -75,20 +75,24 @@ let geneToStructLinks = "";
 // });
 
 async function fetchGenes(organism) {
+  // list of genes
   let { esearchresult } = await $.get(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=gene&term=${organism}[organism]&retmax=1000&retmode=JSON&key=1c1a030e06f70a410fc507821bb474459d08`)
-
-  $.get(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&term=${organism}[organism]&ID=${esearchresult.idlist}&retmode=JSON&key=1c1a030e06f70a410fc507821bb474459d08`)
+  // Gene to Structure conversion
+  $.get(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=gene&db=structure&id=${esearchresult.idlist}&retmode=JSON&key=1c1a030e06f70a410fc507821bb474459d08`)
     .then(res => {
-      for (let uid of res.result.uids) {
+      console.log(res);
+      let structs = res.linksets[0].linksetdbs[0].links
+      /* for (let uid of res.result.uids) {
         console.log(res.result[uid]);
-      }
+        console.log(result);
+      } */
 
       $('.container').first().append(
         $('<ul>')
           .addClass('proteins-list')
           .append(
             res.result.uids
-              .map(uid => res.result[uid])
+              .map(uid => structs)
               .filter(geneInfo => geneInfo.name !== 'NEWENTRY')
               .map(geneInfo => 
                 $('<li>')
@@ -102,10 +106,11 @@ async function fetchGenes(organism) {
 
 async function fetchProtein(uid) {
   $.ajax({
-      url: `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=gene&db=structure&id=${uid}&retmode=JSON&key=1c1a030e06f70a410fc507821bb474459d08`,
+      url: `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=structure&term=${organism}[organism]&ID=${structs}&retmax=1000&retmode=JSON`,
       method: "GET",
     }).then(function (response) {
       console.log(response);
+      
       // !! This is a list of of links from gene to struct UIDs
       if (!response.linksets[0].hasOwnProperty('linksetdbs')) {
         $('.proteins-list')
@@ -139,7 +144,7 @@ async function fetchProtein(uid) {
           rotate: "right",
           mobilemenu: true,
           showcommand: false,
-          showtitle: false,
+          showtitle: true,
         };
         //! This is where we feed UIDs
         cfg["mmdbid"] = `${geneToStructLinks[0]}`;
